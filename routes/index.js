3066572a092d812e2162535ac72cfe3e597273fa
Dereemii <leshermira@gmail.com>
2020-09-11 23:17:36 -0300

    agrego libreria de materialize

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mi primera app con Express' }); /* Es el titulo que se pasa al archivo ejs */
+  res.render('index', { title: 'Express' }); /* Es el titulo que se pasa al archivo ejs */
 });
 
 module.exports = router;
